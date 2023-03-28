756411a5e7578e241ba0a37c8e312701efe5b971
伍和义 <wuheyi@cmhi.chinamobile.com>
2023-03-28 16:53:18 +0800

    修改赞助信息

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+    <span>赞助</span>
+    <a href="https://github.com/wuheyi" target="_blank" class="text-blue-500">
       GitHub
     </a>
   </div>
